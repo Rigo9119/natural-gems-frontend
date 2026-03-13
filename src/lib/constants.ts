02826738e70570bb09dc57abaefe2f1afcb1f5d8
Rigo Rosero Castillo <rigo4791@gmail.com>
2026-03-13 16:30:52 -0500

    delete 75 duplicate files

--- src/lib/constants.ts
+++
@@ -1,5 +0,0 @@
-export const WHATSAPP_NUMBER = "573001234567";
-export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}`;
-export const INSTAGRAM_URL = "https://instagram.com/naturagems";
-export const COMPANY_NAME = "Natura Gems";
-export const COMPANY_LOCATION = "Bogotá, Colombia";
